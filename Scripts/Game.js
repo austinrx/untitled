32d2fd25eb066e59281fa130db38c0ad003a06f1
Austin <jediaustinx@gmail.com>
2015-09-06 13:12:14 -0500

    Fist commit.

--- Scripts/Game.js
+++ Scripts/Game.js
@@ -2,3 +2,12 @@
  * Created by jedia on 9/6/2015.
  */
 console.log('hello');
+var canvasWidth = 800;
+var canvasHeight = 600;
+
+$('#gameCanvas').attr('width',canvasWidth);
+$('#gameCanvas').attr('height',canvasHeight);
+
+var canvas = $('#gameCanvas')[0].getContext('2d');
+
+canvas.strokeRect(0,0 , canvasWidth , canvasHeight);
