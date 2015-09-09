cffda1ca2ec9c8c36d2dcaec4852b642acac4019
Austin <jediaustinx@gmail.com>
2015-09-09 17:03:48 -0500

    Fist commit.

--- Scripts/Game.js
+++ Scripts/Game.js
@@ -9,36 +9,29 @@ $('#gameCanvas').attr('width',canvasWidth);
 $('#gameCanvas').attr('height',canvasHeight);
 
 var canvas = $('#gameCanvas')[0].getContext('2d');
-
 var keysDown = {};
 
 $('body').bind('keydown', function(e){
     keysDown[e.which] = true
-})
+});
 
 $('body').bind('keyup',function(e) {
    keysDown[e.which] = false;
-}
+    });
 
 var image  = new Image();
 image.src = "player.png";
 
-
 $(image).load(function() {
 
-
     canvas.drawImage(image,  (canvasWidth / 2) - (image.width /2), (canvasHeight /2 ) - (image.height /2));
-
 });
 
-
-}
 function draw() {
     console.log('draw');
-    canvas.clearRect(0,0 , canvasWidth,canvasHeight);
-    canvas.strokeRect(0,0 , canvasWidth , canvasHeight);
-
-    canvas.drawImage(image,  playerX,playerY);
+    canvas.clearRect(0, 0, canvasWidth, canvasHeight);
+    canvas.strokeRect(0, 0, canvasWidth, canvasHeight);
+    canvas.drawImage(image, playerX, playerY);
 }
 
 var playerX = (canvasWidth / 2) - (image.width /2);
@@ -49,7 +42,7 @@ var FPS = 30;
 setInterval(function() {
     update();
     draw();
-}, 1000/ FPS)
+}, 1000/ FPS);
 
 function update() {
     console.log('update');
@@ -60,8 +53,18 @@ function update() {
         playerY -= 10;
     }
     if(keysDown[39]) {
-        playerX -= 10;
-        if(keysDown[40]) {
-            playerY -= 10;
-        }
+        playerX += 10;
     }
+
+    if(keysDown[40]) {
+        playerY += 10;
+    }
+
+    playerX = clamp(playerX, 0, canvasWidth - image.width);
+    playerY = clamp(playerY, 0, canvasHeight - image.height);
+}
+
+function clamp(x, min, max) {
+    return x < min ? min : (x > max ? max : x)
+}
+
